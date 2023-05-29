9c6671cfc46d51c271ac485d4ed174ba0c3d0565
zaraizer <95173602+zaraizer@users.noreply.github.com>
2023-05-29 13:20:21 +0300

    Add files via upload

---
+++ var2/src/main/java/com/example/var2/Var2Application.java
@@ -0,0 +1,13 @@
+package com.example.var2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Var2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Var2Application.class, args);
+	}
+
+}
